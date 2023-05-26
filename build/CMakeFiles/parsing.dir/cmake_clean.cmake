8740796e9f631f685427b7dc0ae87d3df6b6002b
Ethan Ho <hoea2@uci.edu>
2023-05-25 20:53:53 -0700

    setting up the environment

---
+++ build/CMakeFiles/parsing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parsing.dir/src/parsing.cpp.o"
+  "CMakeFiles/parsing.dir/src/parsing.cpp.o.d"
+  "parsing"
+  "parsing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parsing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+// #include<iostream>
+
+// int main() {
+//     if (__cplusplus == 202101L) std::cout << "C++23";
+//     else if (__cplusplus == 202002L) std::cout << "C++20";
+//     else if (__cplusplus == 201703L) std::cout << "C++17";
+//     else if (__cplusplus == 201402L) std::cout << "C++14";
+//     else if (__cplusplus == 201103L) std::cout << "C++11";
+//     else if (__cplusplus == 199711L) std::cout << "C++98";
+//     else std::cout << "pre-standard C++." << __cplusplus;
+//     std::cout << "\n";
+//     return 0;
+// }
+
+#include <iostream>
+class X
+{
+public: int i;
+      // first X constructor
+   X(int ia)
+   {
+      i = ia;
+   };
+      // second X constructor
+   X(int ib, int jb)
+   {
+      i = ib + jb;
+   };
+       // X member function myfunc()
+   void myfunc()
+   {
+      i *= 2;
+   };
+      // X destructor
+   ~X()
+   {
+      int id;
+      id = i / 2;
+      if (id < 7 || id > 10)
+      printf("Error - Out of range\n");
+   };
+};
+int main()
+{
+   X *x1 = new X(7);
+   X *x2 = new X(6,2);
+   X *x3 = new X(9);
+   X *x4 = new X(9,1);
+   x1->myfunc();
+   x2->myfunc();
+   x3->myfunc();
+   x4->myfunc();
+   delete x1;
+   delete x2;
+   delete x3;
+   delete x4;
+   return 0;
+}
